046073df0051924fa32bdc712ae291f97da12cd5
Ryan Cheley <rcheley@gmail.com>
2020-01-03 08:41:56 -0800

    Solved Bite097

--- Bite097/Bite097.py
+++ Bite097/Bite097.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 import os
 from urllib.request import urlretrieve
 
+
 from bs4 import BeautifulSoup
 
 
@@ -24,4 +25,25 @@ def get_us_bank_holidays(content=content):
     """Receive scraped html output, make a BS object, parse the bank
        holiday table (css class = list-table), and return a dict of
        keys -> months and values -> list of bank holidays"""
-    pass
+    soup = BeautifulSoup(content, 'html.parser')
+    table_list = []
+    for lt in soup.find_all("table", {"class": "list-table"}):
+        for row in lt.find_all("tr"):
+            table_list.append(row)
+    data = []
+    for t in table_list:
+        try:
+            data.append(
+                (
+                    t.find("time").text[5:7],
+                    t.find("a").text.strip()
+                )
+            )
+        except AttributeError:
+            pass
+    for k, v in data:
+        holidays[k].append(v)
+    return holidays['01']
+
+x = get_us_bank_holidays(content)
+print(x)

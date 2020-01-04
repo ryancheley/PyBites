8b51fe38ddde04805f87cac884a6d2bc2953c2f4
Ryan Cheley <rcheley@gmail.com>
2020-01-04 11:51:31 -0800

    Work on Bite027 ... but no really progress

--- Bite027/Bite027.py
+++ Bite027/Bite027.py
@@ -1,9 +1,24 @@
 import json
+import os
+from urllib.request import urlretrieve
 
+TMP = os.getenv("TMP", "/tmp")
+S3 = 'https://bites-data.s3.us-east-2.amazonaws.com/'
+DATA = 'omdb_data'
+MOVIES = os.path.join(TMP, DATA)
+if not os.path.isfile(MOVIES):
+    urlretrieve(os.path.join(S3, DATA), MOVIES)
 
 def get_movie_data(files: list) -> list:
     """Parse movie json files into a list of dicts"""
-    pass
+    result = []
+    with open(files, 'r') as file:
+        for line in file:
+            result.append(line.split())
+    return result
+
+m = get_movie_data(MOVIES)
+print(m[0])
 
 
 def get_single_comedy(movies: list) -> str:

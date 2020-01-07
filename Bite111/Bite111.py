53eb666d6e9ba38e074fe47abbe4efd34c4e8bf6
Ryan Cheley <rcheley@gmail.com>
2020-01-06 18:27:16 -0800

    Initial Commit of Bite002

--- Bite111/Bite111.py
+++ Bite111/Bite111.py
@@ -6,4 +6,11 @@ IPINFO_URL = 'http://ipinfo.io/{ip}/json'
 def get_ip_country(ip_address):
     """Receives ip address string, use IPINFO_URL to get geo data,
        parse the json response returning the country code of the IP"""
-    pass
+    url = IPINFO_URL.replace('{ip}', ip_address)
+    r = requests.get(url).json()
+    result = r.get('country')
+    return result
+
+
+ip = get_ip_country('185.161.200.10')
+print(ip)

0322e96f089d94cc1de5c79084792707ed43219b
Ryan Cheley <rcheley@gmail.com>
2020-01-30 21:27:50 -0800

    my regex is much more complex than the answer AND the answer works much better. I totally forced this one, but darn it if I could not figure out the last regex needed

---
+++ Bite002/Bite002.py
@@ -0,0 +1,57 @@
+import re
+
+COURSE = ('Introduction 1 Lecture 01:47'
+          'The Basics 4 Lectures 32:03'
+          'Getting Technical!  4 Lectures 41:51'
+          'Challenge 2 Lectures 27:48'
+          'Afterword 1 Lecture 05:02')
+TWEET = ('New PyBites article: Module of the Week - Requests-cache '
+         'for Repeated API Calls - http://pybit.es/requests-cache.html '
+         '#python #APIs')
+HTML = ('<p>pybites != greedy</p>'
+        '<p>not the same can be said REgarding ...</p>')
+TWEET2 = ('PyBites My Reading List | 12 Rules for Life - #books that expand the mind! http://pbreadinglist.herokuapp.com/books/TvEqDAAAQBAJ#.XVOriU5z2tA.twitter #psychology #philosophy')
+
+
+def extract_course_times(course=COURSE):
+    """Return the course timings from the passed in
+       course string. Timings are in mm:ss (minutes:seconds)
+       format, so taking COURSE above you would extract:
+       ['01:47', '32:03', '41:51', '27:48', '05:02']
+       Return this list.
+    """
+    return re.findall('[0-9]{2}:[0-9]{2}', course)
+
+
+def get_all_hashtags_and_links(tweet=TWEET):
+    """Get all hashtags and links from the tweet text
+       that is passed into this function. So for TWEET
+       above you need to extract the following list:
+       ['http://pybit.es/requests-cache.html',
+        '#python',
+        '#APIs']
+       Return this list.
+    """
+    result = []
+    potential = re.findall('(\#[a-zA-Z]*)|(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+(\#.[\S]*)?)',tweet)
+    for i in potential:
+        for j in i:
+            if j!= '':
+                result.append(j)
+    try:
+        result.pop(result.index('#.XVOriU5z2tA.twitter'))
+    except:
+        pass
+
+    return result
+
+g = get_all_hashtags_and_links(TWEET)
+print(g)
+
+def match_first_paragraph(html=HTML):
+    """Extract the first paragraph of the passed in
+       html, so for HTML above this would be:
+       'pybites != greedy' (= content of first paragraph).
+       Return this string.
+    """
+    return re.findall('<p>(.*?)</p>', html)[0]

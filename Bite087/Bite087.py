fbcfd9319368dfadd49797ae272f65e5f9b12a48
Ryan Cheley <rcheley@gmail.com>
2020-02-09 12:08:38 -0800

    Not as hard as i thought

--- Bite087/Bite087.py
+++ Bite087/Bite087.py
@@ -1,22 +1,32 @@
-import collections
+# Inspiration from https://www.w3resource.com/python-exercises/class-exercises/python-class-exercise-1.php
 
 def romanize(decimal_number):
     """Takes a decimal number int and converts its Roman Numeral str"""
-    result = ''
-    roman = collections.OrderedDict()
-    roman['1'] = 'I'
-    roman['5'] = 'V'
-    roman['10'] = 'X'
-    roman['50'] = 'L'
-    roman['100'] = 'C'
-    roman['500'] = 'D'
-    roman['1000'] = 'M'
+    if type(decimal_number) == str:
+        raise ValueError
     if decimal_number <= 0 or decimal_number >= 4000:
         raise ValueError
 
+    val = [
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4,
+        1
+    ]
+    syb = [
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV",
+        "I"
+    ]
+    roman_num = ''
+    i = 0
+    while decimal_number > 0:
+        for _ in range(decimal_number // val[i]):
+            roman_num += syb[i]
+            decimal_number -= val[i]
+        i += 1
+    return roman_num
 
-
-
-
-r = romanize(42)
+r = romanize(1000)
 print(r)

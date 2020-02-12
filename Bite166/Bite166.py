922ee891e80a2dcb0af83d32f14a6ef1ea37080c
Ryan Cheley <rcheley@gmail.com>
2020-02-11 18:33:56 -0800

    oop is a bit harder when you start in the middle

---
+++ Bite166/Bite166.py
@@ -0,0 +1,32 @@
+import configparser
+import re
+
+class ToxIniParser:
+
+    def __init__(self, ini_file):
+        """Use configparser to load ini_file into self.config"""
+        config = configparser.ConfigParser()
+        self.config = config.read(ini_file)
+
+    @property
+    def number_of_sections(self):
+        """Return the number of sections in the ini file.
+           New to properties? -> https://pybit.es/property-decorator.html
+        """
+        return len(re.findall(r'\[.*\]', self.config))
+
+    @property
+    def environments(self):
+        """Return a list of environments
+           (= "envlist" attribute of [tox] section)"""
+        return re.search(r'envlist = .*', self.config).group().replace('envlist = ', '').split(',')
+
+    @property
+    def base_python_versions(self):
+        """Return a list of all basepython across the ini file"""
+        pass
+
+
+t = ToxIniParser
+
+print(t)

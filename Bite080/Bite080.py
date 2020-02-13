96874cbbf194825f7e746e11e19d78abd4913192
Ryan Cheley <rcheley@gmail.com>
2020-02-12 18:23:06 -0800

    Not too shabby

--- Bite080/Bite080.py
+++ Bite080/Bite080.py
@@ -18,4 +18,23 @@ def check_equality(list1, list2):
        - return SAME_UNORDERED_DEDUPED if they have the same unordered content
          and reduced to unique items
        - return NO_EQUALITY if none of the previous cases match"""
-    pass
+    result = ''
+    if list1 is list2:
+        result = Equality.SAME_REFERENCE
+    elif list1 == list2:
+        result = Equality.SAME_ORDERED
+    elif sorted(list1) == sorted(list2):
+        result = Equality.SAME_UNORDERED
+    elif set(list1) == set(list2):
+        result = Equality.SAME_UNORDERED_DEDUPED
+    else:
+        result = Equality.NO_EQUALITY
+
+    return result
+
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+
+d = check_equality(a, b)
+print(d)

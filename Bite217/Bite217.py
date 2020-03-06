6ee7487af8fba8098eb257441f00436411235ebd
Ryan Cheley <rcheley@gmail.com>
2020-03-05 20:08:24 -0800

    fun with builtins

---
+++ Bite217/Bite217.py
@@ -0,0 +1,22 @@
+from contextlib import redirect_stdout
+from io import StringIO
+from types import BuiltinFunctionType
+
+
+def get_len_help_text(builtin: BuiltinFunctionType) -> int:
+    """Receives a builtin, and returns the length of its help text.
+       You need to redirect stdout from the help builtin.
+       If the the object passed in is not a builtin, raise a ValueError.
+    """
+    if isinstance(builtin, BuiltinFunctionType):
+        f = StringIO()
+        with redirect_stdout(f):
+            help(builtin)
+
+        s = len(f.getvalue())
+        return s
+    else:
+        raise ValueError
+
+g = get_len_help_text('max')
+print(g)

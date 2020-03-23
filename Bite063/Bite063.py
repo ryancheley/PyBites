64c3891298c5e5d2336ff078d688262c8299f13c
Ryan Cheley <rcheley@gmail.com>
2020-03-22 18:48:35 -0700

    dude ... traffic lights and infinte generators

---
+++ Bite063/Bite063.py
@@ -0,0 +1,22 @@
+from collections import namedtuple
+from itertools import cycle, islice
+from time import sleep
+
+State = namedtuple('State', 'color command timeout')
+
+
+def traffic_light():
+    """Returns an itertools.cycle iterator that
+       when iterated over returns State namedtuples
+       as shown in the Bite's description"""
+    green = State(color='green', command='Go', timeout=2)
+    amber = State(color='amber', command='Caution', timeout=0.5)
+    red = State(color='red', command='Stop', timeout=2)
+    return cycle([red, green, amber])
+
+
+if __name__ == '__main__':
+    # print a sample of 10 states if run as standalone program
+    for state in islice(traffic_light(), 10):
+        print(f'{state.command}! The light is {state.color}')
+        sleep(state.timeout)

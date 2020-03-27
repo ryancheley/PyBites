ffc22fdcb426d33a72069e54a4e65941f4907e0b
Ryan Cheley <rcheley@gmail.com>
2020-03-26 20:19:16 -0700

    mmm ... maybe next time

---
+++ Bite088/Bite088.py
@@ -0,0 +1,23 @@
+from collections import Counter
+from contextlib import contextmanager
+from datetime import date
+from time import time
+
+OPERATION_THRESHOLD_IN_SECONDS = 2.2
+ALERT_THRESHOLD = 3
+ALERT_MSG = 'ALERT: suffering performance hit today'
+
+violations = Counter()
+
+
+def get_today():
+    """Making it easier to test/mock"""
+    return date.today()
+
+
+@contextmanager
+def timeit():
+    now = date.today()
+    return now - get_today()
+
+print(timeit())

6018b2c7bf84a74a0b83910d7f4fe49ba08ddbf1
Ryan Cheley <rcheley@gmail.com>
2020-03-29 21:20:25 -0700

    did this one with a little help from a friend

--- Bite179/Bite179.py
+++ Bite179/Bite179.py
@@ -5,10 +5,18 @@ def hello_world():
     # A simple comment preceding a simple print statement
     print("Hello World")
 '''
+single_comment_after_strip = '''
+def hello_world():
+    print("Hello World")
+'''
 
 single_docstring = '''
 def say_hello(name):
     """A simple function that says hello... Richie style"""
+    print(f"Hello {name}, is it me you're looking for?")
+'''
+single_docstring_after_strip = '''
+def say_hello(name):
     print(f"Hello {name}, is it me you're looking for?")
 '''
 
@@ -20,18 +28,101 @@ class SimpleClass:
         """Class method docstrings go here."""
         print(f'Hello {name}')
 '''
+class_with_method_after_strip = '''
+class SimpleClass:
+
+    def say_hello(self, name: str):
+        print(f'Hello {name}')
+'''
+
+multiline_docstring = '''
+def __init__(self, name, sound, num_legs):
+    """
+    Parameters
+    ----------
+    name : str
+        The name of the animal
+    sound : str
+        The sound the animal makes
+    num_legs : int, optional
+        The number of legs the animal (default is 4)
+    """
+    self.name = name
+    self.sound = sound
+    self.num_legs = num_legs
+'''
+multiline_docstring_after_strip = '''
+def __init__(self, name, sound, num_legs):
+    self.name = name
+    self.sound = sound
+    self.num_legs = num_legs
+'''
+
+code_bite_description = '''
+"""this is
+my awesome script
+"""
+# importing modules
+import re
+
+def hello(name):
+    """my function docstring"""
+    return f'hello {name}'  # my inline comment
+'''
+code_bite_description_after_strip = '''
+import re
+
+def hello(name):
+    return f'hello {name}'
+'''
+
+class_three_indents = '''
+class SimpleClass:
+    """Class docstrings go here."""
+
+    def say_hello(self, name: str):
+        """Class method docstrings go here."""
+        print(f'Hello {name}')
+
+        def func_in_method(self):
+            """Docstring with 3 indents and multiline
+               should also be stripped
+            """
+            pass
+'''
+class_three_indents_after_strip = '''
+class SimpleClass:
+
+    def say_hello(self, name: str):
+        print(f'Hello {name}')
+
+        def func_in_method(self):
+            pass
+'''
+
+false_positive = '''
+def foo():
+    # this is a comment
+    print('this is not a #comment')
+'''
+false_positive_after_strip = '''
+def foo():
+    print('this is not a #comment')
+'''
+
 
 def strip_comments(code):
-    result_list = []
-    simple_search = re.compile(r'\#.*\n|\"{3}.*\"{3}\n')
-    simple_comment = re.findall(simple_search, code)
-    for i in code.split('\n'):
-        for j in simple_comment:
-            if j.strip() != i.strip():
-                result_list.append(i)
-    return '\n'.join(result_list)
+    code = re.sub(r"#\s+.*\n\s*|\s{2}#\s.*$", r"", code)
+    code = re.sub(r'\s+\"{3}.*\"{3}', r'', code)
+    code = re.sub(r'\"{3}.*\"{3}\n\s*', r'', code, re.MULTILINE, re.DOTALL)
 
+    return code
 
+c = strip_comments(multiline_docstring)
+print(c)
 
-s = strip_comments(class_with_method)
-print(s)
+c = strip_comments(single_comment)
+print(c)
+
+c = strip_comments(single_docstring)
+print(c)

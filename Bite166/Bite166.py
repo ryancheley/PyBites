3cf42f4066175d818a44c9c2132f72d5aa249e66
Ryan Cheley <rcheley@gmail.com>
2020-04-19 07:32:45 -0700

    parsing Tox files for the win

--- Bite166/Bite166.py
+++ Bite166/Bite166.py
@@ -7,27 +7,34 @@ class ToxIniParser:
     def __init__(self, ini_file):
         """Use configparser to load ini_file into self.config"""
         config = configparser.ConfigParser()
-        self.config = config.read(ini_file)
+        config.read(ini_file)
+        self.config = config
+
 
     @property
     def number_of_sections(self):
         """Return the number of sections in the ini file.
            New to properties? -> https://pybit.es/property-decorator.html
         """
-        return len(re.findall(r'\[.*\]', self.config))
+        return len(self.config) - 1
+
 
     @property
     def environments(self):
         """Return a list of environments
            (= "envlist" attribute of [tox] section)"""
-        return re.search(r'envlist = .*', self.config).group().replace('envlist = ', '').split(',')
+        enviroment = sorted([i.lstrip() for i in re.split(r'\n| ,|,', self.config['tox']['envlist']) if i])
+        return enviroment
+
 
     @property
     def base_python_versions(self):
         """Return a list of all basepython across the ini file"""
-        pass
-
-
-t = ToxIniParser
-
-print(t)
+        basepython = []
+        for c in self.config.sections():
+            try:
+                if self.config[c]['basepython'] not in basepython:
+                    basepython.append(self.config[c]['basepython'])
+            except KeyError:
+                pass
+        return basepython

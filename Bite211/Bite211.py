4fc8a12b6910cc597f9671ad2c96a856dac99fe0
Ryan Cheley <rcheley@gmail.com>
2020-06-14 08:00:12 -0700

    Initial Commit of Bite270

---
+++ Bite211/Bite211.py
@@ -0,0 +1,26 @@
+from functools import wraps
+
+MAX_RETRIES = 3
+
+
+class MaxRetriesException(Exception):
+    pass
+
+
+def retry(func):
+    """Complete this decorator, make sure
+       you print the exception thrown"""
+    # ... retry MAX_RETRIES times
+    # ...
+    # make sure you include this for testing:
+    # except Exception as exc:
+    #     print(exc)
+    # ...
+    # and use wraps to preserve docstring
+    #
+    try:
+        1>0
+    except Exception as exc:
+        print(exc)
+
+    pass

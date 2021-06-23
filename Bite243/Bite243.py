46c0d28fe3a5bc6d736819da2b2c794dda1e1228
Ryan Cheley <rcheley@gmail.com>
2021-06-22 18:08:11 -0700

    that was hard

--- Bite243/Bite243.py
+++ Bite243/Bite243.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-from ipaddress import IPv4Network
+from ipaddress import IPv4Network, IPv4Address
 from urllib.request import urlretrieve
 
 import pytest
@@ -21,4 +21,33 @@ def json_file():
     return PATH
 
 
-# write your pytest code ...
+def test_parse_ipv4_service_ranges(json_file):
+    actual = parse_ipv4_service_ranges(json_file)
+    assert len(actual) == 1886
+
+
+def test_service_ip_range_string():
+    service = 'AMAZON'
+    region = 'ap-east-1'
+    cidr = '54.240.17.0/24'
+    actual_my_service_ip_range = str(ServiceIPRange(service, region, cidr))
+    expected_my_service_ip_range = '54.240.17.0/24 is allocated to the AMAZON service in the ap-east-1 region'
+    assert actual_my_service_ip_range == expected_my_service_ip_range
+
+
+def test_get_aws_service_range_no_error():
+    service = 'AMAZON'
+    region = 'ap-east-1'
+    cidr = IPv4Network('54.240.17.0/24')
+    ip_range = [ServiceIPRange(service, region, cidr), ]
+    actual = get_aws_service_range('54.240.17.5', ip_range)
+    expected = '54.240.17.5'
+    print(actual)
+    print(expected)
+    assert actual == ip_range
+
+
+def test_get_aws_service_range_error():
+    with pytest.raises(ValueError, match="Address must be a valid IPv4 address"):
+        get_aws_service_range('a.b.c.d', ['a'])
+
